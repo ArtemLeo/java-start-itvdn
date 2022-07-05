7cf39947ea8e9a62475b62797c201dcd57079f24
ArtemLeo <link.by.artem@gmail.com>
2022-07-05 16:46:28 +0300

    Update files

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        // На 6-й строке, выводим фразу - Hello World, на экран.
+
+        System.out.println("Hello World!");
+    }
+}
