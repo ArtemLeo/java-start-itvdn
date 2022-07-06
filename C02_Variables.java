65197b964f98e85a5717ab96b0be3ccd8703b8a2
ArtemLeo <link.by.artem@gmail.com>
2022-07-06 17:05:07 +0300

    Update files

---
+++ C02_Variables.java
@@ -0,0 +1,11 @@
+public class C02_Variables {
+    public static void main(String[] args) {
+        // На 7-й строке, создаем переменную с именем a, типа byte и присваиваем ей значение 2.
+
+        byte a = 2;
+
+        // На 11-й строке, выводим значение переменной - a, на экран.
+
+        System.out.println(a);
+    }
+}
--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-
-        // На 6-й строке, выводим фразу - Hello World, на экран.
-
-        System.out.println("Hello World!");
-    }
-}
