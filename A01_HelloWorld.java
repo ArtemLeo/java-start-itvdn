57d7409af45d70441b9e614d742335e483a97ec0
ArtemLeo <link.by.artem@gmail.com>
2022-07-23 14:48:15 +0300

    Update files

--- A01_HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class A01_HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("HelloWorld!");
-
-    }
-
-}
--- B04_FloatDouble.java
+++
@@ -1,9 +0,0 @@
-public class B04_FloatDouble {
-    public static void main(String[] args) {
-        float variable1 = 0.12345678901234567890f;
-        double variable2 = 0.12345678901234567d;
-
-        System.out.println(variable1);
-        System.out.println(variable2);
-    }
-}
