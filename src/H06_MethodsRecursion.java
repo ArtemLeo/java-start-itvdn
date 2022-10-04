16134737d3b6a1796aa06a79b5d78e1531f51456
ArtemLeo <105164053+ArtemLeo@users.noreply.github.com>
2022-10-04 20:25:24 +0300

    Update H06_MethodsRecursion.java

--- src/H06_MethodsRecursion.java
+++ src/H06_MethodsRecursion.java
@@ -1,23 +1,20 @@
 public class H06_MethodsRecursion {
+    
     // Рекурсия (сложная рекурсия).
     // Сложная рекурсия -  вызов методом себя, через другой метод.
 
     static void recursion(int counter) {
         counter--;
-
         System.out.println("Первая половина метода recursion: " + counter);
 
         if (counter != 0)
             method(counter);
-
         System.out.println("Вторая половина метода recursion: " + counter);
     }
 
     static void method(int counter) {
         System.out.println("Первая половина метода method: " + counter);
-
         recursion(counter);
-
         System.out.println("Вторая половина метода method: " + counter);
     }
 
