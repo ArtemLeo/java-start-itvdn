d09618fbfcaf745cb7073d9e89c51dec06bcd366
ArtemLeo <105164053+ArtemLeo@users.noreply.github.com>
2022-10-04 20:23:08 +0300

    Update H04_MethodsMainOverload.java

--- src/H04_MethodsMainOverload.java
+++ src/H04_MethodsMainOverload.java
@@ -1,13 +1,14 @@
 public class H04_MethodsMainOverload {
     public static void main(String[] args) {
-        if(args.length > 0)  // если через консоль были введены аргументы
-            System.out.println(args[0]);  //то вывести на консоль первый элемент из массива
-        else {  //иначе —
-            main(); // и использовать обычный метод с названием main()
+        
+        if(args.length > 0)               // если через консоль были введены аргументы
+            System.out.println(args[0]);  // то вывести на консоль первый элемент из массива
+        else {                            // иначе - использовать обычный метод с названием main()
+            main();
         }
     }
 
-    public static void main() {  // это обычный метод с названием main()
+    public static void main() {          
         System.out.println("it's usual main method without String[] args!");
     }
 }
