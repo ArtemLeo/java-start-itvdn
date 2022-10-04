3d8a9fd18caac54b048749925b1bf381b0724760
ArtemLeo <105164053+ArtemLeo@users.noreply.github.com>
2022-10-04 20:20:01 +0300

    Update H02_MethodsNamedArgs.java

--- src/H02_MethodsNamedArgs.java
+++ src/H02_MethodsNamedArgs.java
@@ -1,17 +1,17 @@
 public class H02_MethodsNamedArgs {
+    
     // Именованные аргументы методов
-
     static int difference(int height, int width) {
         return height - width;
     }
 
     public static void main(String[] args) {
+        
         // Можно вызвать метод так:
         int difference;
         difference = difference(6, 5);
 
         // difference = difference(5, 6);
-
         System.out.println("Разность равна: " + difference);
     }
 }
