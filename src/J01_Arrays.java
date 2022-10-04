9848ee82d275c8d3d17ec53ee21991e039008297
ArtemLeo <105164053+ArtemLeo@users.noreply.github.com>
2022-10-04 20:30:36 +0300

    Update J01_Arrays.java

--- src/J01_Arrays.java
+++ src/J01_Arrays.java
@@ -2,12 +2,11 @@ public class J01_Arrays {
     // Массивы (одномерный массив).
 
     public static void main(String[] args) {
-        // На 9 строке создаем массив целочисленных элементов с именем array размерностью в 5 элементов
-
+        
+        // Создаем массив целочисленных элементов с именем array размерностью в 5 элементов
         int[] array = new int[5];
 
         // На 13 строке элементу массива array по индексу 0 присваиваем значение 10
-
         array[0] = 10;
         array[1] = 20;
         array[2] = 30;
@@ -15,7 +14,6 @@ public class J01_Arrays {
         array[4] = 50;
 
         // На 21 строке выводим на экран значение элемента массива array по индексу 0
-
         System.out.println(array[0]);
         System.out.println(array[1]);
         System.out.println(array[2]);
