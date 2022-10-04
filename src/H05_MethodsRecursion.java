bc5398a610605a694103a99af95e486e28b3d4ec
ArtemLeo <105164053+ArtemLeo@users.noreply.github.com>
2022-10-04 20:24:31 +0300

    Update H05_MethodsRecursion.java

--- src/H05_MethodsRecursion.java
+++ src/H05_MethodsRecursion.java
@@ -6,12 +6,10 @@ public class H05_MethodsRecursion {
 
     static void recursion(int counter) {
         counter--;
-
         System.out.println("Первая половина метода: " + counter);
 
         if (counter != 0)
             recursion(counter);
-
         System.out.println("Вторая половина метода: " + counter);
     }
 
