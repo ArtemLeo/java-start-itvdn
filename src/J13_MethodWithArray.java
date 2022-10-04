fc3986af2b44ac782895ae74ac20d03e6a965916
ArtemLeo <105164053+ArtemLeo@users.noreply.github.com>
2022-10-04 20:43:29 +0300

    Update J13_MethodWithArray.java

--- src/J13_MethodWithArray.java
+++ src/J13_MethodWithArray.java
@@ -1,6 +1,5 @@
 public class J13_MethodWithArray {
     static int[] modifyArray(int[] array, int modifier) {
-        
         for (int i = 0; i < array.length; i++) {
             array[i] = array[i] * modifier;
         }
@@ -10,7 +9,6 @@ public class J13_MethodWithArray {
     public static void main(String[] args) {
         int[] myArray = {1, 2, 3, 4, 5};
         myArray = modifyArray(myArray, 5);
-
         for (int i = 0; i < myArray.length; i++) {
             System.out.print(myArray[i] + ", ");
         }
