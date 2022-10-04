bfe4eec62d8473efd37801c997938336f30944f7
ArtemLeo <105164053+ArtemLeo@users.noreply.github.com>
2022-10-04 19:44:24 +0300

    Update D02_IfElse.java

--- src/D02_IfElse.java
+++ src/D02_IfElse.java
@@ -2,7 +2,7 @@ public class D02_IfElse {
     public static void main(String[] args) {
         int a = 1, b = 2;
 
-        if (a < b) { // Если условие удовлетворяет истинности, выполняем тело блока if.
+        if (a < b) {   // Если условие удовлетворяет истинности, выполняем тело блока if.
             System.out.println("a < b");               // Ветвь 1
         } else {       // Иначе, выполняем тело блока else.
             System.out.println("a не меньше b");       // Ветвь 2
